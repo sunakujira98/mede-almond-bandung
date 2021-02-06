e277c0c7d63c360aeacc1a6598f24d173c455cb5
Steven Theodore <steventheodore98@gmail.com>
2021-02-06 22:33:08 +0700

    href in Top product to whatsapp

--- src/components/Feature/index.js
+++ src/components/Feature/index.js
@@ -6,7 +6,15 @@ const Feature = () => {
     <FeatureContainer>
       <h1>Top kacang bulan ini</h1>
       <p>Almond Premium</p>
-      <FeatureButton>Pesan Sekarang</FeatureButton>
+      <a
+        target='_blank'
+        href={
+          'https://api.whatsapp.com/send?phone=6281809091567&text=Halo%20Admin.%20Mau%20pesan%20Almond%20Premium%20nih.'
+        }
+        rel='noreferrer'
+      >
+        <FeatureButton>Pesan Sekarang</FeatureButton>
+      </a>
     </FeatureContainer>
   )
 }
