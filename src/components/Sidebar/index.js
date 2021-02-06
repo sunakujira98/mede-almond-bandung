3f93a1e3e773a55902b9709198a0e6f7c1f2c1dd
Steven Theodore <steventheodore98@gmail.com>
2021-02-06 22:38:15 +0700

    change sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -16,12 +16,12 @@ const Sidebar = ({ isOpen, toggle }) => {
         <CloseIcon />
       </Icon>
       <SidebarMenu>
-        <SidebarLink to='/'>Pizzas</SidebarLink>
-        <SidebarLink to='/'>Desserts</SidebarLink>
-        <SidebarLink to='/'>Full Menu</SidebarLink>
+        <SidebarLink to='/'>Almond</SidebarLink>
+        <SidebarLink to='/'>Mede</SidebarLink>
+        <SidebarLink to='/'>Lokasi</SidebarLink>
       </SidebarMenu>
       <SideBtnWrap>
-        <SidebarRoute to='/'>Order Now</SidebarRoute>
+        <SidebarRoute to='/'>Pesan Sekarang</SidebarRoute>
       </SideBtnWrap>
     </SidebarContainer>
   )
